643f09e83f3a38e28626b843d75b92fb53d969c6
Mahin Utsman Nawawi <mahin@riyadlulhuda.com>
2026-01-10 21:40:52 +0700

    feat: update favicon with foundation logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, user-scalable=yes">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Dashboard') - Riyadlul Huda</title>
+    <link rel="icon" href="{{ asset('favicon.ico') }}">
     
     <!-- CSS -->
     <link rel="stylesheet" href="{{ asset('css/design-system.css') }}">
